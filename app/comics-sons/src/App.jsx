2dc106fe0f044b5b88a41297c3df3122ad45fb7b
marcecalle <38355063+marcecalle@users.noreply.github.com>
2024-03-14 16:46:25 -0300

    Implementación de router-react-dom, sección de "Nosotros" y "Contacto"

--- app/comics-sons/src/App.jsx
+++ app/comics-sons/src/App.jsx
@@ -1,18 +1,22 @@
-import logoComicsSons from './assets/logo-comics-sons.svg'
+import './App.css'
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import NavBar from "./components/NavBar/NavBar"
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AboutUsContainer from './components/AboutUs/AboutUsContainer'
+import ContactContainer from './components/ContactContainer/ContactContainer'
 
 function App() {
   
   return (
     <>
-      <header className='header-container'>
-        <img className='logo-img' src={logoComicsSons} alt="Logo comics sons" />
-        <h1>COMICS SONS</h1>
+      <BrowserRouter>
         <NavBar />
-      </header>
-      <ItemListContainer greeting='Bienvenido'/>
+        <Routes>
+          <Route exact path='/' element={<AboutUsContainer />} />
+          <Route exact path='/products' element={<ItemListContainer greeting='Bienvenido'/>} />
+          <Route exact path='/contact' element={<ContactContainer />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ app/comics-sons/src/components/ContactContainer/ContactContainer.jsx
@@ -0,0 +1,12 @@
+import './ContactContainer.css'
+
+export default function ContactContainer() {
+
+   return(
+      <>
+         <section className='contact-container'>
+            <h3 className='contact-title'>Espacio en construcción</h3>
+         </section>
+      </>
+   )
+}
